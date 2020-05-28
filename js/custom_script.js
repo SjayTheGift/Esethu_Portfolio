7aa7e1cd9b4c17e185e611b89016cd97d16337b8
Ejonas <jonas.gift83@gmail.com>
2020-05-28 05:27:23 +0200

    first commit

---
+++ js/custom_script.js
@@ -0,0 +1,60 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+jQuery(function ($) {
+        var styles = [{"featureType": "landscape", "stylers": [{"saturation": -100}, {"lightness": 65}, {"visibility": "on"}]}, {"featureType": "poi", "stylers": [{"saturation": -100}, {"lightness": 51}, {"visibility": "simplified"}]}, {"featureType": "road.highway", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "road.arterial", "stylers": [{"saturation": -100}, {"lightness": 30}, {"visibility": "on"}]}, {"featureType": "road.local", "stylers": [{"saturation": -100}, {"lightness": 40}, {"visibility": "on"}]}, {"featureType": "transit", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "administrative.province", "stylers": [{"visibility": "off"}]}, {"featureType": "water", "elementType": "labels", "stylers": [{"visibility": "on"}, {"lightness": -25}, {"saturation": -100}]}, {"featureType": "water", "elementType": "geometry", "stylers": [{"hue": "#ffff00"}, {"lightness": -25}, {"saturation": -97}]}];
+    map = new GMaps({
+	el: '#map1',
+	lat: -34.063953060737205,
+	lng: 18.653321350000056,
+	zoomControl: true,
+	zoomControlOpt: {
+	    style: 'SMALL',
+	    position: 'TOP_LEFT'
+	},
+	panControl: false,
+	streetViewControl: false,
+	mapTypeControl: false,
+	overviewMapControl: false,
+	scrollwheel: false,
+	draggable: false,
+	styles: styles
+    });
+
+    var image = 'img/marker.png';
+
+    map.addMarker({
+	lat: -34.063953060737205,
+	lng: 18.653321350000056,
+        animation: google.maps.Animation.BOUNCE,
+	icon: image ,
+	 title: 'Home',
+	 infoWindow: {
+	 content: '<p id="find-me">Find me here</p>'
+	 }
+    });
+ $("#contact-form").submit(function () {
+
+	var url = "contact.php"; // the script where you handle the form input.
+
+	$.ajax({
+	    type: "POST",
+	    url: url,
+	    data: $(this).serialize(), // serializes the form's elements.
+	    success: function (data)
+	    {
+		var messageAlert = 'alert-' + data.type;
+		var messageText = data.message;
+		var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable animated bounceIn"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+		if (messageAlert && messageText) {
+		    $('#contact-form').find('.messages').html(alertBox);
+		}
+	    }
+	});
+	return false; // avoid to execute the actual submit of the form.
+    });
+ });
+
+
